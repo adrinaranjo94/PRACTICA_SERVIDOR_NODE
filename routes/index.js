88ade0ffe117ac6172ed41bd5b7fccb8e782edc7
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-03-18 12:47:11 +0100

    Validators

--- routes/index.js
+++ routes/index.js
@@ -2,13 +2,38 @@ const express = require("express");
 
 const router = express.Router();
 
+const { check, validationResult } = require("express-validator");
+
 router.get("/", (req, res) => {
   res.render("form", { title: "Registration form" });
 });
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.render("form", { title: "Registration form" });
-});
+router.post(
+  "/",
+  [
+    check("name")
+      .isLength({ min: 1 })
+      .withMessage("Please enter a name"),
+    check("email")
+      .isLength({ min: 1 })
+      .withMessage("Please enter an email"),
+    check("password")
+      .isLength({ min: 5 })
+      .withMessage("Please enter a password")
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (errors.isEmpty()) {
+      res.send("Thank you for your registration!");
+    } else {
+      res.render("form", {
+        title: "Registration form",
+        errors: errors.array(),
+        data: req.body
+      });
+    }
+  }
+);
 
 module.exports = router;
