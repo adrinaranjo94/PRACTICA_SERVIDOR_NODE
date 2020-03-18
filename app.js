bc5e95e0fd069d95da27b1c2f251ea66d2aee4dd
Sergio Redondo Montes <redondomontes.sergio@gmail.com>
2020-03-18 12:27:18 +0100

    gitignore

--- app.js
+++ app.js
@@ -1,55 +1,14 @@
 const express = require("express");
+const path = require("path");
+const routes = require("./routes/index");
+const bodyParser = require("body-parser");
+
 const app = express();
-const cookieParser = require("cookie-parser");
 
-app.use("/static", express.static(__dirname + "/public"));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/", routes);
 
-app.get("/", function(req, res) {
-  res.send("Hello World!");
-});
-
-app.get("/login", (req, res) => {
-  res.send("No tienes acceso al contenido, Inicia sesión");
-});
-
-// RECOGIDA DE PARAMETROS POR URL
-app.get("/nombre/:name", function(req, res) {
-  res.send("Bienvenido " + req.params.name);
-});
-
-// REDIRECCIONAMIENTO
-app.get("/redirect", (req, res) => {
-  res.redirect("/login");
-});
-
-// CREACIÓN COOKIE
-app.get("/app/set-cookie/:value", (req, res) => {
-  res.cookie("cookie-servidor", req.params.value);
-  res.send("Cookie implementada");
-});
-
-// LECTURA COOKIE
-app.get("/app/read-cookie", (req, res) => {
-  res.send("Cookie valor guardado: " + req.cookies["cookie-servidor"]);
-});
-
-// RENDER HTML
-app.get("/html", (req, res) => {
-  res.send("<h1>HOLA</h1><p>Esto es código html</p>");
-});
-
-// RENDER HTML CON PARAMETRO URL
-app.get("/html/:name", (req, res) => {
-  res.send("<h1>HOLA " + req.params.name + " </h1><p>Esto es código html</p>");
-});
-
-// RENDER TEMPLATE
-app.get("/template", (req, res) => {
-  res.sendFile("index.html", { root: "./public" });
-});
-
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Example app listening on port " + process.env.PORT || "3000");
-});
+module.exports = app;
